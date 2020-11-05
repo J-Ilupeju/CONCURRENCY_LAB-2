fb30f3461cec39d5ef0d3e30f2c535763ed12f85
Juliet-Ilupeju <38299861+Juliet-Ilupeju@users.noreply.github.com>
2020-11-05 17:32:51 +0000

    Final Synchronization

--- src/com/company/StatsCounter.java
+++ src/com/company/StatsCounter.java
@@ -3,19 +3,32 @@ package com.company;
 public final class StatsCounter {
 
     private int successCount_;
+    
+    private boolean isIncreaseCount = false;
+    
     public final int getSuccessCount() {
         return successCount_;
     }
     public final synchronized void increaseSuccessCount(int delta) {
-//        try {
-//            wait();
-//        } catch (InterruptedException e) {
-//           // e.printStackTrace();
-//        }
-        successCount_ += delta;
+//       
+        while (isIncreaseCount) {
+       
+       try {
+            System.out.println("Waiting - inserting" + delta);
+            wait();
+       } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        }
+        //successCount_ += delta;
 
-//        notifyAll();
-//        System.out.println( "Done insertion");
+        boolean isIncreasingCount = true;
+        successCount_ += delta;
+        isIncreasingCount = false;
+        
+
+       notifyAll();
+       System.out.println( "Done insertion");
     }
 }
 
